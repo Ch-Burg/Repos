b7c8e5069163a296fa9f5b29e0d84f564f886cf4
Vibe Garden <ch.burg@arcor.de>
2020-08-25 11:05:37 +0200

    Write Basta 2019

---
+++ BustaLuchApp/BustaLuchApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BustaLuchApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello New New World!");
+        }
+    }
+}
